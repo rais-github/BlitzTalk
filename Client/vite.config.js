3b9bcdc889cebb2c4a899925a420fc5fc0c5cd74
Himanshu Rai <himanshu2003rai@gmail.com>
2024-05-13 10:04:09 +0530

    Final commit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": "https://blitztalk-ip09.onrender.com",
     },
   },
 });
